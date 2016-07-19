075b0a68c42d9a4e6304766399f88125e2c04e88
Artyom <yom@artyom.me>
2016-07-19 23:41:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cmark-highlight
+
+[![Hackage](https://img.shields.io/hackage/v/cmark-highlight.svg)](https://hackage.haskell.org/package/cmark-highlight)
+[![Build status](https://secure.travis-ci.org/aelve/cmark-highlight.svg)](https://travis-ci.org/aelve/cmark-highlight)
+[![BSD3 license](https://img.shields.io/badge/license-BSD3-blue.svg)](https://github.com/aelve/cmark-highlight/blob/master/LICENSE)
---
+++ lib/CMark/Highlight.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE
+ViewPatterns,
+RecordWildCards
+  #-}
+
+
+module CMark.Highlight
+(
+  -- * Highlighting
+  highlight,
+  highlightWith,
+
+  -- * Options
+  FormatOptions(..),
+  defaultFormatOpts,
+
+  -- * Styles
+  module Text.Highlighting.Kate.Styles,
+  styleToCss,
+)
+where
+
+
+import qualified Data.Text as T
+import Data.Text (Text)
+import qualified Data.Text.Lazy as TL
+import CMark
+import Text.Highlighting.Kate
+import Text.Highlighting.Kate.Styles
+import Text.Blaze.Html.Renderer.Text
+
+
+{- |
+Highlight a document by replacing code blocks with raw HTML blocks.
+
+By default, the rest of the attribute line (i.e. all words after the first word after @~~~@ or @```@) get added as classes to the container block of the code.
+-}
+highlight :: Node -> Node
+highlight = highlightWith (\_ _ x -> x)
+
+{- |
+The function is given code block's language (i.e. the 1st word of the attribute line after @~~~@ or @```@) and the rest of the attribute line.
+
+If you don't want the classes to be derived from the attribute line, make the function set 'containerClasses' to @[]@.
+-}
+highlightWith
+  :: (Text -> Text -> FormatOptions -> FormatOptions) -> Node -> Node
+highlightWith f (Node pos (CODE_BLOCK info code) ns) =
+    Node pos (HTML_BLOCK formatted) (map (highlightWith f) ns)
+  where
+    (codeLang, T.drop 1 -> codeInfo) = T.break (== ' ') (T.strip info)
+    highlighted = highlightAs (T.unpack codeLang) (T.unpack code)
+    opts = defaultFormatOpts {
+             containerClasses = words (T.unpack codeInfo) }
+    formatted = TL.toStrict . renderHtml $
+      formatHtmlBlock (f codeLang codeInfo opts) highlighted
+highlightWith f (Node pos type_ ns) =
+    Node pos type_ (map (highlightWith f) ns)
