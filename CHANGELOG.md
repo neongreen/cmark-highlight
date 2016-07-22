ac5411565c66df3eff95013fc200596edf0a79b3
Artyom <yom@artyom.me>
2016-07-22 15:50:12 +0300

    Rename highlight to highlightNode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.2.0.0
+
+* Renamed `highlight` to `highlightNode`.
+
 # 0.1.0.0
 
 First release.
--- lib/CMark/Highlight.hs
+++ lib/CMark/Highlight.hs
@@ -6,8 +6,8 @@ ViewPatterns
 module CMark.Highlight
 (
   -- * Highlighting
-  highlight,
-  highlightWith,
+  highlightNode,
+  highlightNodeWith,
 
   -- * Options
   FormatOptions(..),
@@ -34,18 +34,18 @@ Highlight a document by replacing code blocks with raw HTML blocks.
 
 By default, the rest of the attribute line (i.e. all words after the first word after @~~~@ or @```@) get added as classes to the container block of the code.
 -}
-highlight :: Node -> Node
-highlight = highlightWith (\_ _ x -> x)
+highlightNode :: Node -> Node
+highlightNode = highlightNodeWith (\_ _ x -> x)
 
 {- |
 The function is given code block's language (i.e. the 1st word of the attribute line after @~~~@ or @```@) and the rest of the attribute line.
 
 If you don't want the classes to be derived from the attribute line, make the function set 'containerClasses' to @[]@.
 -}
-highlightWith
+highlightNodeWith
   :: (Text -> Text -> FormatOptions -> FormatOptions) -> Node -> Node
-highlightWith f (Node pos (CODE_BLOCK info code) ns) =
-    Node pos (HTML_BLOCK formatted) (map (highlightWith f) ns)
+highlightNodeWith f (Node pos (CODE_BLOCK info code) ns) =
+    Node pos (HTML_BLOCK formatted) (map (highlightNodeWith f) ns)
   where
     (codeLang, T.drop 1 -> codeInfo) = T.break (== ' ') (T.strip info)
     highlighted = highlightAs (T.unpack codeLang) (T.unpack code)
@@ -53,5 +53,5 @@ highlightWith f (Node pos (CODE_BLOCK info code) ns) =
              containerClasses = words (T.unpack codeInfo) }
     formatted = TL.toStrict . renderHtml $
       formatHtmlBlock (f codeLang codeInfo opts) highlighted
-highlightWith f (Node pos type_ ns) =
-    Node pos type_ (map (highlightWith f) ns)
+highlightNodeWith f (Node pos type_ ns) =
+    Node pos type_ (map (highlightNodeWith f) ns)
